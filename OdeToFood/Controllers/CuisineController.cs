07975a72f250edc6285ae20e2779c77a7f775e33
BrainSkel <hanne53rik@gmail.com>
2022-10-05 14:12:01 +0300

    LogAttribute

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -1,16 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Filters;
 using System.Text.Encodings.Web;
 
 namespace OdeToFood.Controllers
 {
+    [Log]
     public class CuisineController : Controller
     {
+        //[Authorize]
         public IActionResult Search( string name="UnKnown")
         {
             var message = HtmlEncoder.Default.Encode(name);
             //return Content(name);
             //return new EmptyResult();
-            return File("/css/site.css", "text/css");
+            //return File("/css/site.css", "text/css");
+            //return Json(HtmlEncoder.Default);
+
+            //return Redirect("https://www.tthk.ee");
+            //return RedirectPermanent("https://www.tthk.ee");
+            return RedirectToRoute("default", new { controller= "Home",action = "About"});
+
+        
         }
+        public IActionResult Second(int count)
+        {
+            return Content($"Teine on siin! {count}");
+            }
     }
 }
---
+++ OdeToFood/Filters/LogAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace OdeToFood.Filters
+{
+    public class LogAttribute :ActionFilterAttribute
+    {
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+        }
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+        }
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            base.OnResultExecuted(context);
+        }
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            base.OnResultExecuting(context);
+        }
+    }
+}
