10a91258e7e8df5e19d426d0bb06eff1c4d64f14
BrainSkel <hanne53rik@gmail.com>
2022-10-14 10:56:14 +0300

    Add Create Review

--- OdeToFood/Migrations/20221014073949_ReviewsId.Designer.cs
+++ OdeToFood/Migrations/20221014073949_ReviewsId.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OdeToFood.Data;
 
 namespace OdeToFood.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221014073949_ReviewsId")]
+    partial class ReviewsId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OdeToFood/Migrations/20221014073949_ReviewsId.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OdeToFood.Migrations
+{
+    public partial class ReviewsId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RestaurantReviewId",
+                table: "RestaurantReviews",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RestaurantReviews_RestaurantReviewId",
+                table: "RestaurantReviews",
+                column: "RestaurantReviewId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RestaurantReviews_RestaurantReviews_RestaurantReviewId",
+                table: "RestaurantReviews",
+                column: "RestaurantReviewId",
+                principalTable: "RestaurantReviews",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RestaurantReviews_RestaurantReviews_RestaurantReviewId",
+                table: "RestaurantReviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RestaurantReviews_RestaurantReviewId",
+                table: "RestaurantReviews");
+
+            migrationBuilder.DropColumn(
+                name: "RestaurantReviewId",
+                table: "RestaurantReviews");
+        }
+    }
+}
