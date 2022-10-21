0d36654f6abbfa1d427ac995ac6fa607be223937
BrainSkel <hanne53rik@gmail.com>
2022-10-21 09:35:50 +0300

    Add Async search

---
+++ OdeToFood/Models/RestaurantListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Models
+{
+    public class RestaurantListViewModel:Restaurant 
+    {
+        public int CountOfReviews { get; set; }
+    }
+}
--- OdeToFood/wwwroot/js/site.js
+++ OdeToFood/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(function () {
+    var ajaxFormSubmit = function () {
+        var $form = $(this);
+
+        var options = {
+            url: $form.atrr("action"),
+            type: $form.attr("method"),
+            data: $form.serialize()
+        }
+
+        $.ajax(options).done(function (data) {
+            var $target = $($form.attr("data-oft-targer"));
+            $target.replace(data);
+        });
+
+        return false;
+    };
+
+    $("form[data-otf-ajax='true']").submit(ajaxFormSubmit);
+})
